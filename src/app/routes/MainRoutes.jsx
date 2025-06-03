6becb0cb0c822a61c43bb6df264eab2e7312fe09
DatTien2107 <chytai2012/2gmail.com>
2025-06-03 11:06:17 +0700

    Update main routes

--- src/app/routes/MainRoutes.jsx
+++ src/app/routes/MainRoutes.jsx
@@ -4,15 +4,20 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Log In'
 import BookDetail from '../pages/BookDetails/BookDetail'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function MainRoutes() {
   return (
+    
     <BrowserRouter>
+    <Header/>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/book/:id" element={<BookDetail/>}/>
         </Routes>
+    <Footer/>
     </BrowserRouter>
     
   )
